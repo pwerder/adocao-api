16a08e76f0cde09085c6a95aaed92e56e2bc06ab
Paulo Antonio <paulo.werder@gmail.com>
2023-04-05 20:12:15 -0300

    create data base

---
+++ database/migrations/2023_04_05_224918_create_tutors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tutors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 20);
+            $table->string('password');
+            $table->string('telephone', 20)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->longText('about')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tutors');
+    }
+};
